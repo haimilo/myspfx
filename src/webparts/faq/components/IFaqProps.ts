bd1ff9fccd02b0a1761be70a99b548a74fc364e7
Harry Nhu <harry.nhu@vmodev.com>
2023-02-08 11:19:36 +0700

    Part1: First create base and config SPFx enviroments

---
+++ src/webparts/faq/components/IFaqProps.ts
@@ -0,0 +1,7 @@
+export interface IFaqProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/faq/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IFaqWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'FaqWebPartStrings' {
+  const strings: IFaqWebPartStrings;
+  export = strings;
+}
